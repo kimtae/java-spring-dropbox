e77d3b759a8b43af9e111ed61a8616ed16b0f8b6
dogbite1337 <kocak_cc25@hotmail.com>
2022-03-17 17:35:21 +0100

    Created file post method

--- src/main/java/com/uppgift/dropbox/FileController.java
+++ src/main/java/com/uppgift/dropbox/FileController.java
@@ -4,17 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.security.Principal;
+
 @CrossOrigin
 @RestController
 public class FileController {
     @Autowired
     private FileRepository fileRepository;
 
+    private static final String ROOT_PATH = System.getProperty("user.dir");
+
     @PostMapping(path="/file")
-    public @ResponseBody String addFile (@RequestBody MultipartFile file) {
+    public @ResponseBody String addFile (@RequestBody MultipartFile file, Principal principal) {
         try {
-            System.out.println(file.getContentType());
-            return null;
+            // Create directory for saving files if necessary
+            File dir = new File(ROOT_PATH + File.separator + "userFiles" +
+                    File.separator + principal.getName());
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            // Create the file on server
+            byte[] bytes = file.getBytes();
+            File serverFile = new File(dir.getAbsolutePath() + File.separator + file.getOriginalFilename());
+            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+            stream.write(bytes);
+            stream.close();
+
+            return "Success";
         } catch (Exception e) {
             return e.toString();
         }
