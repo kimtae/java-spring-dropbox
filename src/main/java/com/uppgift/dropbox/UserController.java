70bc3880b4de66a7982cc59405e29414b5aebab5
dogbite1337 <kocak_cc25@hotmail.com>
2022-03-20 14:34:23 +0100

    Updated user responses

--- src/main/java/com/uppgift/dropbox/UserController.java
+++ src/main/java/com/uppgift/dropbox/UserController.java
@@ -1,6 +1,9 @@
 package com.uppgift.dropbox;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,15 +19,15 @@ public class UserController {
     }
 
     @PostMapping(path="/user")
-    public @ResponseBody User addUser (@RequestParam String username, @RequestParam String password) {
+    public @ResponseBody ResponseEntity<Resource> addUser (@RequestParam String username, @RequestParam String password) {
         if (userRepository.findByUsername(username) != null) {
-            return null;
+            return new ResponseEntity("User Already Exists", HttpStatus.OK);
         } else {
             User user = new User();
             user.setUsername(username);
             user.setPassword(new BCryptPasswordEncoder(10).encode(password));
             userRepository.save(user);
-            return user;
+            return new ResponseEntity("User Created", HttpStatus.OK);
         }
     }
 }
